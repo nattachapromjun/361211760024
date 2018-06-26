b322b8dce1bb8dc184bd46b904ccae3a16cc9aa8
nattachapromjun <nattacha*937@gmail.com>
2018-06-26 11:41:01 +0700

    Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,16 @@
+package lab1;
+
+public class myCode {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(X:"Hello Word.");
+        System.out.println(x:"nattacha promjun");
+        System.out.println(x:"361211760024");
+        System.out.println(x:"MIT211");
+        System.out.println(x:"RMUTSV");
+
+
+    }//main
+    }//class
